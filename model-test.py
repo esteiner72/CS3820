7c22981cbdc0a092079e43cc217d63523a3f03b5
esteiner72 <ethansteiner14@gmail.com>
2024-09-10 16:13:12 -0600

    1

--- model-test.py
+++ model-test.py
@@ -5,9 +5,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+import csv
+from sklearn.utils import Bunch
 
-# Load the Iris dataset
-iris = datasets.load_iris()
+def load_my_fancy_dataset():
+    with open(r'Disease_symptom_and_patient_profile_dataset.csv') as csv_file:
+        data_reader = csv.reader(csv_file)
+        feature_names = next(data_reader)[:-1]
+        data = []
+        target = []
+        for row in data_reader:
+            features = row[:-1]
+            label = row[-1]
+            data.append([float(num) for num in features])
+            target.append(int(label))
+        
+        data = np.array(data)
+        target = np.array(target)
+    return Bunch(data=data, target=target, feature_names=feature_names)
+
+# Load the Iris dataset (150x4 numpy.ndarray matrix with sepal length, sepal width, flower length, flower width)
+iris = datasets.load_my_fancy_dataset()
 X = iris.data  # we only take the first two features.
 y = iris.target
 
