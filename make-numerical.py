9fa32fc9cdbd0192d2a8d665375645f3d6d28a76
esteiner72 <ethansteiner14@gmail.com>
2024-09-10 16:21:51 -0600

    script edit to push to github
    change output for your machine if you do this (but we should only have to do this once)

--- make-numerical.py
+++ make-numerical.py
@@ -12,4 +12,4 @@ mappings = {
 
 df = pd.read_csv('Downloads/Disease_symptom_and_patient_profile_dataset.csv')
 df.replace(mappings, inplace=True)
-df.to_csv('trimmed_disease_data.csv', index=False)
+df.to_csv('Documents/GitHub/CS3820/trimmed_disease_data.csv', index=False)
