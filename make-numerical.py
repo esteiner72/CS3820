57be8b89bdd9837ae1c462cb090b72370a5d9cb3
esteiner72 <ethansteiner14@gmail.com>
2024-09-10 16:24:13 -0600

    Update make-numerical.py

--- make-numerical.py
+++ make-numerical.py
@@ -12,4 +12,5 @@ mappings = {
 
 df = pd.read_csv('Downloads/Disease_symptom_and_patient_profile_dataset.csv')
 df.replace(mappings, inplace=True)
+# Change on your laptop/machine if re-doing process (but we shouldn't need to)
 df.to_csv('Documents/GitHub/CS3820/trimmed_disease_data.csv', index=False)
