af56b5f03efafce3d5820e8649c48ccfef944591
esteiner72 <ethansteiner14@gmail.com>
2024-09-16 15:01:31 -0600

    testing vector similarity code

--- make-numerical.py
+++ make-numerical.py
@@ -23,3 +23,8 @@ df = pd.read_csv('Downloads/disease_data.csv')
 df.replace(mappings, inplace=True)
 # Change on your laptop/machine if re-doing process (but we shouldn't need to)
 df.to_csv('Documents/GitHub/CS3820/trimmed_disease_data.csv', index=False)
+
+df = pd.read_csv('Documents/GitHub/CS3820/trimmed_disease_data.csv')
+df.drop('Asthma', inplace=True, axis=1) 
+df.to_csv('Documents/GitHub/CS3820/trimmed_disease_data2.csv', index=False)
+
--- model-test.py
+++ model-test.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import norm
 import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -7,6 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import csv
 from sklearn.utils import Bunch
+from scipy import spatial
 
 def load_my_fancy_dataset():
     with open(r'Documents/GitHub/CS3820/trimmed_disease_data.csv') as csv_file:
@@ -18,14 +20,25 @@ def load_my_fancy_dataset():
             features = row[:-1]
             label = row[-1]
             data.append([float(num) for num in features])
-            target.append(float(label))
+            target.append(int(label))
         
         data = np.array(data)
         target = np.array(target)
     return Bunch(data=data, target=target, feature_names=feature_names)
 
-load_my_fancy_dataset()
+test = [0, 0, 10, 0, 82, 2, 0]
+data = load_my_fancy_dataset()
+ex_data = data.data[1]
+# to get rid of disease id num in disease data
+ex_data = ex_data[1:]
+print(test)
+print(ex_data)
+# calculating distance between vectors (lists)
+#cos_sim = np.dot(test, ex_data)/(norm(test)*norm(ex_data))
+cos_sim = 1-spatial.distance.cosine(test, ex_data)
+print((cos_sim))
 
+'''
 iris = datasets.load_my_fancy_dataset()
 X = iris.data  # we only take the first two features.
 y = iris.target
@@ -46,4 +59,4 @@ clf.fit(X_train_scaled, y_train)
 y_pred = clf.predict(X_test_scaled)
 
 # Output the accuracy of the classifier
-print('Accuracy: {:.2f}'.format(accuracy_score(y_test, y_pred)))
+print('Accuracy: {:.2f}'.format(accuracy_score(y_test, y_pred)))'''
