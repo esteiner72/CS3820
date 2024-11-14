7f9a6c44bdddc33a900aff5681754c52f4adcfbf
esteiner72 <ethansteiner14@gmail.com>
2024-11-13 20:56:38 -0700

    Front end FIXXX

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CS3820
 semester ai project
+
+1. (Windows) npm install nodemon
+   (Mac/Linux) sudo npm install -g --force nodemon
+
+2. npm install express 
+
+3. To start server:
+    (Windows) npx nodemon
+    (Mac/Linux) nodemon
--- src/server.js
+++ src/server.js
@@ -1,23 +1,36 @@
-// Import
 const express = require('express');
-const route = require('./routes/route');
-const app = express();
+const fs = require('fs');
 const path = require('path');
-
-// Static Files
-app.use(express.static(path.join(__dirname, '../public')));
-app.use('/css', express.static(path.join(__dirname, '../public/css')));
-app.use('/js', express.static(path.join(__dirname, '../public/js')));
-app.use('/img', express.static(path.join(__dirname, '../public/img')));
-
-app.use('/', route);
-
-// Set Views
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// Listen on port 3000
+const app = express();
 const port = 3000;
-app.listen(port, function () {
-  console.log('Bubble node app running on port ' + port);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/data', express.static(path.join(__dirname, '..', 'data')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/submit-data', (req, res) => {
+    const { selectedDataIds } = req.body;
+    const csvData = selectedDataIds.join(',');
+    
+    const userInputFilePath = path.join(__dirname, '..', 'data', 'User_Input.csv');
+    
+    fs.writeFile(userInputFilePath, csvData, 'utf8', (err) => {
+        if (err) {
+            console.error('Failed to write to User_Input.csv:', err);
+            return res.status(500).send('Failed to save data');
+        }
+        res.send('Data saved successfully');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
